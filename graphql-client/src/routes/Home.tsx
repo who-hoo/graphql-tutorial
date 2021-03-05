b2d529177ecea3daf22eebbdfb3e2e57e13aa597
who-hoo <who.ho3ov@gmail.com>
2021-03-06 01:42:48 +0900

    Feat : 홈화면 GET_MOVIES

--- graphql-client/src/routes/Home.tsx
+++ graphql-client/src/routes/Home.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  query {
+    movies(limit: 10, rating: 9.0) {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <div>
       <h1>Home</h1>
+      {!loading &&
+        !error &&
+        data.movies &&
+        data.movies.map((movie: { id: number }) => (
+          <ul key={movie.id}>{movie.id}</ul>
+        ))}
     </div>
   );
 };
